eafc245fc6533108cfa079242f69bcbbd54904cc
Team4239 <warpspeed@willmar.org>
2019-01-17 16:48:20 -0600

    Update CargoIntake.java

--- src/main/java/frc/robot/subsystems/CargoIntake.java
+++ src/main/java/frc/robot/subsystems/CargoIntake.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,7 +8,7 @@ import frc.robot.commands.CargoIntakeStop;
 
 public class CargoIntake extends Subsystem {
 
-  private WPI_VictorSPX cargoIntakeBottomMotor = new WPI_VictorSPX(RobotMap.cargoIntakeMotor);
+  private WPI_VictorSPX cargoIntakeMotor = new WPI_VictorSPX(RobotMap.cargoIntakeMotor);
 
   @Override
   public void initDefaultCommand() {
@@ -17,17 +16,14 @@ public class CargoIntake extends Subsystem {
   }
 
   public void cargoIntakeStop() {
-    cargoIntakeBottomMotor.set(0.0);
-    cargoIntakeBottomMotor.set(0.0);
+    cargoIntakeMotor.set(0.0);
   }
 
   public void cargoIntakeIn() {
-    cargoIntakeTopMotor.set(0.5);
-    cargoIntakeBottomMotor.set(-0.5);
+    cargoIntakeMotor.set(-0.5);
   }
 
   public void cargoIntakeOut () {
-    cargoIntakeTopMotor.set(-0.5);
-    cargoIntakeBottomMotor.set(0.5);
+    cargoIntakeMotor.set(0.5);
   }
 }
